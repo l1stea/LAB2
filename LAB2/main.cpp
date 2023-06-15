2648c17d11ff17b0b1088f0c82e7567bfd4c0782
Elisey Timoshenko <devru70@gmail.com>
2023-06-15 22:35:23 +0700

    Выполненные задачи: 8, 9 ЛР №1

--- LAB2/main.cpp
+++ LAB2/main.cpp
@@ -1,26 +1,64 @@
 #include "main.h"
-#include <iostream> // для cin и cout
+#include <iostream> // cin и cout
 #include <Windows.h> // Обязательно для SetConsoleCP() и SetConsoleOutputCP()
-#include <stdlib.h> // для std
+#include <stdlib.h>
 #include <string.h>
+using namespace std;
+
 int main()
 {
 	SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
 	SetConsoleOutputCP(1251); // Вывод на консоль в кодировке 1251.
 
 
-	char test[] = "Тимошенко ЕМ";
-	char test2[] = "Косова АЕ";
-	char test3[] = "ООП";
+    char nameSt[] = "Тимошенко ЕМ";
+	char nameEx[] = "Косова АЕ";
+	char nameSb[] = "ООП";
+
 	time_t t = time(nullptr);
 	int dt = t;
 	int gd = 5;
-	Exam ex(test, test2, test3, dt, gd);
-	Exam exam;
-	ex.Print();
 
-	//&test - адрес
-	//*test - значение указателя
-	//test - значение по адресу
-	//test++ увеличивает адрес в указателе
+	Exam exFull(nameSt, nameEx, nameSb, dt, gd);
+	Exam exEmpty;
+
+	exFull.Print();
+
+
 }
+
+void Exam::Print()
+{
+    cout << "ФИО студента: " << nameStudent << endl;
+    cout << "ФИО экзаменатора: " << nameExaminer << endl;
+    cout << "Предмет: " << nameSubject << endl;
+    time_t t = date;
+    tm* now = localtime(&t);
+    cout << "Дата: " << now->tm_mday << '/' << (now->tm_mon + 1) << '/'
+        << (now->tm_year + 1900) << endl;
+    cout << "Оценка: " << grade << endl;
+}
+
+
+Exam::Exam()
+{
+    nameStudent = nullptr;
+    nameExaminer = nullptr;
+    nameSubject = nullptr;
+    time_t t = time(nullptr);
+    date = t;
+    grade = 2;
+};
+
+Exam::Exam(char nameSt[],
+    char nameEx[],
+    char nameSb[],
+    int dt,
+    int gd)
+{
+    strcpy(nameStudent, nameSt);
+    strcpy(nameExaminer, nameEx);
+    strcpy(nameSubject, nameSb);
+    date = dt;
+    grade = gd;
+};
--- LAB2/main.h
+++ LAB2/main.h
@@ -2,15 +2,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <ctime> // Для time_t, tm
 #include <iostream>
-
+int nameLength = 20;
 
 
 class Exam
 {
 private:
-    char* nameStudent;
-    char* nameExaminer;
-    char* nameSubject;
+    char* nameStudent = new char[nameLength];
+    char* nameExaminer = new char[nameLength];
+    char* nameSubject = new char [nameLength];
     int date;
     int grade;
 public:
@@ -21,41 +21,4 @@ public:
         int grade);
     Exam();
     void Print();
-};
-
-
-void Exam::Print() 
-{
-    std::cout << "ФИО студента: " << nameStudent << "\n";
-    std::cout << "ФИО экзаменатора: " << nameExaminer << "\n";
-    std::cout << "Предмет: " << nameSubject << "\n";
-    std::time_t t = date;
-    std::tm* now = std::localtime(&t);
-    std::cout << "Дата: " << now->tm_mday << '/' << (now->tm_mon + 1) << '/'
-        << (now->tm_year + 1900) << std::endl;
-    std::cout << "Оценка: " << grade << "\n";
-}
-
-
-Exam::Exam()
-{
-    nameStudent = nullptr;
-    nameExaminer = nullptr;
-    nameSubject = nullptr;
-    std::time_t t = std::time(nullptr);
-    date = t;
-    grade = 2;
-};
-
-Exam::Exam(char nameSt[],
-    char nameEx[],
-    char nameSb[],
-    int dt,
-    int gd)
-{
-    nameStudent = nameSt;
-    nameExaminer = nameEx;
-    nameSubject = nameSb;
-    date = dt;
-    grade = gd;
 };
